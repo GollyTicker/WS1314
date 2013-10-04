4ff2a095fa49659699935df8fad6862ba4f8c489
GollyTicker <golly.ticker@gmail.com>
2013-10-04 23:13:16 +0200

    moved GKA project into a subfolder to allow multiple projects to coexist

---
+++ GKA/GraphADTws1314/test/TestPackage/AIGraphTest.java
@@ -0,0 +1,47 @@
+package TestPackage;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import graphadtws1314.JavaParser;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import graphadtws1314.AIGraph;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+/**
+ *
+ * @author Swaneet
+ */
+public class AIGraphTest {
+    
+    public AIGraphTest() {
+    }
+        
+        
+    @Test
+    public void test1() throws FileNotFoundException, IOException {
+        //new AIGraph();
+        assertTrue(true);
+        
+        JavaParser jp = new JavaParser("C:\\Users\\Swaneet\\Desktop\\HAW\\graph_01.graph");
+        AIGraph yolo = jp.createGraph();
+        
+        //System.out.println(yolo.toString());
+        
+        assertTrue(yolo.getVertexNames().contains("Augsburg"));
+        assertTrue(!yolo.isEmpty());
+        int augsburgID = 0; // Augsburg (src), eId1 = 0, 70km
+        int munichID = 1; // München (target)
+        assertEquals(augsburgID, yolo.getSource(0));
+        assertNotSame(munichID, yolo.getSource(0));
+        assertEquals(munichID, yolo.getTarget(0));
+        assertNotSame(augsburgID, yolo.getTarget(0));
+        
+        
+        
+    }
+}
