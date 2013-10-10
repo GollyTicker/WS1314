f5c963fb62f6b20ece0fc2a68fc504a5f0e87483
GollyTicker <golly.ticker@gmail.com>
2013-10-10 11:38:30 +0200

    finalized interface

--- AD/SwaneetMatthias/src/ab2_adts/Matrix.java
+++ AD/SwaneetMatthias/src/ab2_adts/Matrix.java
@@ -1,14 +1,14 @@
 package ab2_adts;
 
 public interface Matrix {
-
-	public Matrix add(Matrix m);
 	
-	public Matrix mul(double skalar);
+	void insert(int i, int j, double value);
 	
-	public Matrix mul(Matrix factor);
+	Matrix add(Matrix m);
 	
-	public Matrix pow(int exponent);
-	//Die Grundoperationen einer Matrix sind die Initialisierung (wie), die Addition, die ska-
-	// lare Multiplikation, die (Matrix-)Multiplikation und die Potenzierun
+	Matrix mul(double skalar);
+	
+	Matrix mul(Matrix factor);
+	
+	Matrix pow(int exponent);
 }
