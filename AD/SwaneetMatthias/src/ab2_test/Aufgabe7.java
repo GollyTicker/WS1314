fd63995aacce249a798bc808ddd184cdcd7a46e9
GollyTicker <golly.ticker@gmail.com>
2013-10-12 15:25:20 +0200

    almost finished 7+8; part (d) incomprehended

--- AD/SwaneetMatthias/src/ab2_test/Aufgabe7.java
+++ AD/SwaneetMatthias/src/ab2_test/Aufgabe7.java
@@ -1,7 +1,5 @@
 package ab2_test;
 
-import static org.junit.Assert.*;
-
 import java.text.DecimalFormat;
 import java.util.Arrays;
 
@@ -13,32 +11,35 @@ import ab2_adts.*;
 import java.util.*;
 
 public class Aufgabe7 {
-	
-	// CAREFUL!! On overly overfilled and slightly old laptops like mine(Swaneet's) 
+
+	// CAREFUL!! On overly overfilled and slightly old laptops like
+	// mine(Swaneet's)
 	// already the n=450, t=5 experiment takes 415 seconds - 7mins!
-	
+
 	private final static int n = 120; // N x N is the size of the matrices
-	private final static int t = 5; // t the number of randomly generated matrices
-	private final static List<Double> pValues = new ArrayList<>(Arrays.asList(0.01,
-			0.05, 0.1));
+	private final static int t = 5; // t the number of randomly generated
+									// matrices
+	private final static List<Double> pValues = new ArrayList<>(Arrays.asList(
+			0.01, 0.05, 0.1));
 
 	private static long start = 0;
 
 	private final static DecimalFormat df = new DecimalFormat("#.####");
-	
-	private static String getDuration(){
-		return " - " + df.format((System.nanoTime() - start)/1000000.0) + " ms";
+
+	private static String getDuration() {
+		return " - " + df.format((System.nanoTime() - start) / 1000000.0)
+				+ " ms";
 	}
-	
+
 	@Test
 	public void test() {
 
 		start = System.nanoTime();
-		
+
 		String acc = "Output: \n";
 		acc += "n = " + n + "\n";
 		acc += "t = " + t + "\n";
-		
+
 		System.out.println("Started experiments" + getDuration());
 		// repeat for each propabilit of a non-zero element
 		for (Double pNonZero : pValues) {
@@ -78,70 +79,69 @@ public class Aufgabe7 {
 		// Platz_p(t):
 		System.out.println("Finisched" + getDuration());
 		System.out.println(acc);
-		
-//		Output 1:
-//			n = 150
-//			t = 5
-//			Average NoOfElements for p = 0.01:
-//			mList - 235.0
-//			mArrayList - 235.0
-//
-//
-//			Average NoOfElements for p = 0.05:
-//			mList - 1139.8
-//			mArrayList - 1139.8
-//
-//
-//			Average NoOfElements for p = 0.1:
-//			mList - 2239.6
-//			mArrayList - 2239.6
-		
-		
+
+		// Output 1:
+		// n = 150
+		// t = 5
+		// Average NoOfElements for p = 0.01:
+		// mList - 235.0
+		// mArrayList - 235.0
+		//
+		//
+		// Average NoOfElements for p = 0.05:
+		// mList - 1139.8
+		// mArrayList - 1139.8
+		//
+		//
+		// Average NoOfElements for p = 0.1:
+		// mList - 2239.6
+		// mArrayList - 2239.6
+
 		// Output - 2:
-//		Started experiments - 0,0902 ms
-//		p = 0.01 started  - 0,8292 ms
-//		p = 0.05 started  - 2163,8838 ms
-//		p = 0.1 started  - 12288,6113 ms
-//		Finisched - 32391,8897 ms
-//		Output: 
-//		n = 250
-//		t = 5
-//		Average NoOfElements for p = 0.01:
-//		mList - 623.4
-//		mArrayList - 623.4
-//
-//
-//		Average NoOfElements for p = 0.05:
-//		mList - 3084.0
-//		mArrayList - 3084.0
-//
-//
-//		Average NoOfElements for p = 0.1:
-//		mList - 6294.4
-//		mArrayList - 6294.4
-		
+		// Started experiments - 0,0902 ms
+		// p = 0.01 started - 0,8292 ms
+		// p = 0.05 started - 2163,8838 ms
+		// p = 0.1 started - 12288,6113 ms
+		// Finisched - 32391,8897 ms
+		// Output:
+		// n = 250
+		// t = 5
+		// Average NoOfElements for p = 0.01:
+		// mList - 623.4
+		// mArrayList - 623.4
+		//
+		//
+		// Average NoOfElements for p = 0.05:
+		// mList - 3084.0
+		// mArrayList - 3084.0
+		//
+		//
+		// Average NoOfElements for p = 0.1:
+		// mList - 6294.4
+		// mArrayList - 6294.4
+
 		// Output - 3:
-//		Started experiments - 0,0988 ms
-//		p = 0.01 started  - 0,9331 ms
-//		p = 0.05 started  - 17599,0173 ms
-//		p = 0.1 started  - 121828,4797 ms
-//		Finisched - 415058,773 ms
-//		Output: 
-//		n = 450
-//		t = 5
-//		Average NoOfElements for p = 0.01:
-//		mList - 2016.2
-//		mArrayList - 2016.2
-//
-//
-//		Average NoOfElements for p = 0.05:
-//		mList - 10150.6
-//		mArrayList - 10150.6
-//
-//
-//		Average NoOfElements for p = 0.1:
-//		mList - 20218.4
-//		mArrayList - 20218.4
+		// Started experiments - 0,0988 ms
+		// p = 0.01 started - 0,9331 ms
+		// p = 0.05 started - 17599,0173 ms
+		// p = 0.1 started - 121828,4797 ms
+		// Finisched - 415058,773 ms
+		// Output:
+		// n = 450
+		// t = 5
+		// Average NoOfElements for p = 0.01:
+		// mList - 2016.2
+		// mArrayList - 2016.2
+		//
+		//
+		// Average NoOfElements for p = 0.05:
+		// mList - 10150.6
+		// mArrayList - 10150.6
+		//
+		//
+		// Average NoOfElements for p = 0.1:
+		// mList - 20218.4
+		// mArrayList - 20218.4
 	}
 
 }
--- AD/SwaneetMatthias/src/ab2_test/Aufgabe8.java
+++ AD/SwaneetMatthias/src/ab2_test/Aufgabe8.java
@@ -1,7 +1,5 @@
 package ab2_test;
 
-import static org.junit.Assert.*;
-
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -14,29 +12,36 @@ import ab2_adts.*;
 
 public class Aufgabe8 {
 
-	private final static int n = 100; // N x N is the size of the matrices
-	private final static int t = 5; // t the number of randomly generated
+	private final static int n = 60; // N x N is the size of the matrices
+	private final static int t = 15; // t the number of randomly generated
 									// matrices
 	private final static List<Double> pValues = new ArrayList<>(Arrays.asList(
-			0.001, 0.005, 0.01, 0.05, 0.1, 0.5));
+			0.01, 0.05, 0.1));
 
 	private static long start = 0;
 
 	private final static DecimalFormat df = new DecimalFormat("#.####");
+	private final static DecimalFormat dfAccessCount = new DecimalFormat(
+			"000000000.######");
 
 	private static String getDuration() {
 		return " - " + df.format((System.nanoTime() - start) / 1000000.0)
 				+ " ms";
 	}
 
+	private static String niceFormat(double val) {
+		return dfAccessCount.format(val);
+	}
+
 	@Test
 	public void test() {
-		start = System.nanoTime();			// count global time because it takes all sooooo long :O
+		start = System.nanoTime(); // count global time because it takes all
+									// sooooo long :O
 
 		String acc = "Output: \n";
 		acc += "n = " + n + "\n";
 		acc += "t = " + t + "\n";
-		
+
 		Matrix summand = new MatrixList(n, n);
 		Matrix factor = new MatrixList(n, n);
 
@@ -66,52 +71,126 @@ public class Aufgabe8 {
 				// fill matrices in the different implementations
 				mList.copyFrom(m);
 				mArrayList.copyFrom(m);
-				
+
 				// reset and initialize for measurements
 				mList.resetAccessCount();
 				mArrayList.resetAccessCount();
-				
-				
-				
+
 				// measuring addition on List
 				mList.add(summand);
-				int mListAddAccessCount = mList.accessCount();							// ###
+				int mListAddAccessCount = mList.accessCount(); // ###
 				mList.resetAccessCount();
-				
 
 				// measuring multiplication on List
 				mList.mul(factor);
-				int mListMulAccessCount = mList.accessCount();							// ###
+				int mListMulAccessCount = mList.accessCount(); // ###
 				mList.resetAccessCount();
-				
-				
+
 				// measuring addition on ArrayList
 				mArrayList.add(summand);
-				int mArrayListAddAccessCount = mList.accessCount();						// ###
+				int mArrayListAddAccessCount = mArrayList.accessCount(); // ###
 				mArrayList.resetAccessCount();
-				
 
 				// measuring multiplication on ArrayList
 				mArrayList.mul(factor);
-				int mArrayListMulAccessCount = mList.accessCount();						// ###
+				int mArrayListMulAccessCount = mArrayList.accessCount(); // ###
 				mArrayList.resetAccessCount();
-				
-				
+
 				statsListAdd.addValue(mListAddAccessCount);
 				statsListMul.addValue(mListMulAccessCount);
 				statsArrayListAdd.addValue(mArrayListAddAccessCount);
 				statsArrayListMul.addValue(mArrayListMulAccessCount);
 			}
 
-//			acc += "Average NoOfElements for p = " + pNonZero + ":\n";
-//			acc += "mList - " + statsList.getAverage() + "\n";
-//			acc += "mArrayList - " + statsArrayList.getAverage() + "\n";
-//			acc += "\n\n";
+			acc += "AccessCounts For pNonZero = " + pNonZero + ":\n";
+			acc += "mList:\n";
+			acc += "  Addition:\n";
+			acc += "    Average: " + niceFormat(statsListAdd.getAverage())
+					+ "\n";
+			acc += "    Variance: " + niceFormat(statsListAdd.getVariance())
+					+ "\n";
+			acc += "  Multiplication:\n";
+			acc += "    Average: " + niceFormat(statsListMul.getAverage())
+					+ "\n";
+			acc += "    Variance: " + niceFormat(statsListMul.getVariance())
+					+ "\n";
+			acc += "mArrayList:\n";
+			acc += "  Addition:\n";
+			acc += "    Average: " + niceFormat(statsArrayListAdd.getAverage())
+					+ "\n";
+			acc += "    Variance: "
+					+ niceFormat(statsArrayListAdd.getVariance()) + "\n";
+			acc += "  Multiplication:\n";
+			acc += "    Average: " + niceFormat(statsArrayListMul.getAverage())
+					+ "\n";
+			acc += "    Variance: "
+					+ niceFormat(statsArrayListMul.getVariance()) + "\n";
+			acc += "\n\n";
 
 		}
 		// Platz_p(t):
 		System.out.println("Finisched" + getDuration());
 		System.out.println(acc);
 	}
+	
+	
+	// Output:
+//			Started experiments - 2,1462 ms
+//			p = 0.01 started  - 2,638 ms
+//			p = 0.05 started  - 1645,0155 ms
+//			p = 0.1 started  - 7765,8316 ms
+//			Finisched - 19866,8312 ms
+//			Output: 
+//			n = 60
+//			t = 15
+//			AccessCounts For pNonZero = 0.01:
+//			mList:
+//			  Addition:
+//			    Average: 000142015,666667
+//			    Variance: 474503863,666667
+//			  Multiplication:
+//			    Average: 008520940
+//			    Variance: 1708213909200
+//			mArrayList:
+//			  Addition:
+//			    Average: 000002365,933333
+//			    Variance: 000131114,066667
+//			  Multiplication:
+//			    Average: 000141956
+//			    Variance: 472010640
+//		
+//		
+//			AccessCounts For pNonZero = 0.05:
+//			mList:
+//			  Addition:
+//			    Average: 000622421
+//			    Variance: 2912721758,142857
+//			  Multiplication:
+//			    Average: 037345260
+//			    Variance: 10485798329314,285
+//			mArrayList:
+//			  Addition:
+//			    Average: 000010383
+//			    Variance: 000822370
+//			  Multiplication:
+//			    Average: 000622980
+//			    Variance: 2960532000
+//		
+//		
+//			AccessCounts For pNonZero = 0.1:
+//			mList:
+//			  Addition:
+//			    Average: 001249198,733333
+//			    Variance: 2165148774,066667
+//			  Multiplication:
+//			    Average: 074951924
+//			    Variance: 7794535586640
+//			mArrayList:
+//			  Addition:
+//			    Average: 000020835,866667
+//			    Variance: 000593188,838095
+//			  Multiplication:
+//			    Average: 001250152
+//			    Variance: 2135479817,142857
 
 }
