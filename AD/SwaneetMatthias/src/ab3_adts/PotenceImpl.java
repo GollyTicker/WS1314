c6889d44bf7cb4e12739a3a7c857aeab125a99e7
Matthias Nitsche <matthias@ws-91-245.haw-1x.haw-hamburg.de>
2013-10-16 19:26:30 +0200

    potence resolves

--- AD/SwaneetMatthias/src/ab3_adts/PotenceImpl.java
+++ AD/SwaneetMatthias/src/ab3_adts/PotenceImpl.java
@@ -2,6 +2,14 @@ package ab3_adts;
 
 public class PotenceImpl {
 
+	/**
+	 * Takes an Integer x to the power of k
+	 * 
+	 * @param x
+	 *            Base integer
+	 * @param k
+	 *            exponent
+	 */
 	public static int exp(int x, int k) {
 		int r = 1;
 		for (int i = 0; i < k; i++)
@@ -9,14 +17,20 @@ public class PotenceImpl {
 		return r;
 	}
 
-	// x^0 = 1
-	// x^2n = (x^n)^2
-	// x^2n+1 = x*(x^n)^2
-	// public static int exp2(int x, int k) {
-	// int r = 1;
-	// for (int i = 0; i < k; i++)
-	// r = r * x;
-	// return r;
-	// }
+	/**
+	 * Takes an Integer x to the power of k
+	 * 
+	 * @param x
+	 *            Base integer
+	 * @param k
+	 *            exponent
+	 */
+	public static int expFast(int x, int k) {
+		int r = 0;
+		// x^0 = 1
+		// x^2n = (x^n)^2
+		// x^2n+1 = x*(x^n)^2
+		return r;
+	}
 
 }
