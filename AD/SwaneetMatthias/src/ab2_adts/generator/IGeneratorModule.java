262c3ba5c9a1b38c032ef72dfa8b20ea5d0bfce8
Matthias Nitsche <matthias@ws-94-112.haw-1x.haw-hamburg.de>
2013-10-17 17:07:33 +0200

    comments

--- AD/SwaneetMatthias/src/ab2_adts/generator/IGeneratorModule.java
+++ AD/SwaneetMatthias/src/ab2_adts/generator/IGeneratorModule.java
@@ -4,12 +4,16 @@ import ab2_adts.matrix.Matrix;
 
 public interface IGeneratorModule {
 
+	// get the current Randomized Matrix
 	Matrix getMatrix();
 
+	// create a new Randomized Matrix and overrwrite the old one
 	void generateRandomMatrix();
 
+	// count all zeros in the current matrix (absolute)
 	int countZero();
 
+	// ratio between zeros and values in decimal <= 1.0
 	double getRatio();
 
 }
