c901d697c18889ed6353994bc70366384ffeb38d
Matthias Nitsche <matthias@ws-88-87.haw-1x.haw-hamburg.de>
2013-10-17 09:09:12 +0200

    potence

--- AD/SwaneetMatthias/src/ab3_adts/PotenceImpl.java
+++ AD/SwaneetMatthias/src/ab3_adts/PotenceImpl.java
@@ -1,36 +1,68 @@
 package ab3_adts;
 
+import java.util.List;
+import java.util.ArrayList;
+
+import ab1_adts.statmodule.AverageVariance;
+
 public class PotenceImpl {
 
+	public static void main(String[] args) {
+		long max = 60;	// the limit of the java long type is 2^60
+		List<Long> expL = new ArrayList<>();
+		List<Long> expFastL = new ArrayList<>();
+		for (long i = 0; i < max; i++) {
+			long x = 2;
+			long k = i;
+			accessCountNormal = 0;
+			accessCountFast = 0;
+			long exp1 = exp(x, k);
+			long exp2 = expFast(x, k);
+			expL.add(accessCountNormal);
+			expFastL.add(accessCountFast);
+		}
+		System.out.println(expL);
+		System.out.println(expFastL);
+	}
+
+	public static long accessCountNormal = 0;
+
 	/**
 	 * Takes an Integer x to the power of k
 	 * 
 	 * @param x
-	 *            Base integer
+	 *            Base Integer
 	 * @param k
 	 *            exponent
 	 */
-	public static int exp(int x, int k) {
-		int r = 1;
-		for (int i = 0; i < k; i++)
+	public static long exp(long x, long k) {
+		long r = 1;
+		for (long i = 0; i < k; i++) {
+			accessCountNormal++;
 			r = r * x;
+		}
 		return r;
 	}
+
+	public static long accessCountFast = 0;
 
 	/**
 	 * Takes an Integer x to the power of k
 	 * 
 	 * @param x
-	 *            Base integer
+	 *            Base Integer
 	 * @param k
 	 *            exponent
 	 */
-	public static int expFast(int x, int k) {
-		int r = 0;
-		// x^0 = 1
-		// x^2n = (x^n)^2
-		// x^2n+1 = x*(x^n)^2
-		return r;
+	public static long expFast(long x, long k) {
+		accessCountFast++;
+		if (k == 0)
+			return 1;
+		if (k % 2 == 0) {
+			long tmp = expFast(x, k / 2);
+			return tmp * tmp;
+		}
+		long tmp = expFast(x, (k - 1) / 2);
+		return tmp * tmp * x;
 	}
-
 }
