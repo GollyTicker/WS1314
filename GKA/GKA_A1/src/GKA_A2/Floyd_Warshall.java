60c82590699fb5eb6200f03255affb6e33ad07c5
Matthias Nitsche <matthias@Matthiass-MacBook-Pro-2.local>
2013-10-26 12:38:06 +0200

    matrix added for shortest path

--- GKA/GKA_A1/src/GKA_A2/Floyd_Warshall.java
+++ GKA/GKA_A1/src/GKA_A2/Floyd_Warshall.java
@@ -1,6 +1,25 @@
 package GKA_A2;
 
+import GKA_A1.IAIGraph;
+
 public class Floyd_Warshall {
-	
-	
+
+	private IAIGraph graph;
+	private boolean isDirected;
+
+	public Floyd_Warshall(IAIGraph graph, boolean isDirected) {
+		this.graph = graph;
+		this.isDirected = isDirected;
+	}
+
+	public void initialize() {
+
+	}
+
+	// Shortest Path from -> to
+	public String getPath(long v1, long v2) {
+		String accu = "";
+		return "";
+	}
+
 }
