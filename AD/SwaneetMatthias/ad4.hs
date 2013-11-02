ab75dc6df8b4a4300339ff2fd7bcfb9f6404a132
GollyTicker <golly.ticker@gmail.com>
2013-11-02 12:46:37 +0100

    aufgabe 4.1 fin

--- AD/SwaneetMatthias/ad4.hs
+++ AD/SwaneetMatthias/ad4.hs
@@ -1,2 +1,12 @@
-f 0 = 2
+f 0 = 2	
 f n = 2 + 3 * f (n-1)
+
+a n = 3 ^ n
+b n = 3 ^ (n+1)
+c n = 3 ^ (n+1) - 1
+
+
+m n = ("F:", f n, ("A:", a n), ("B:", b n), ("C:", c n))
+
+
+main = putStrLn $ show (map (m) [0..7])
