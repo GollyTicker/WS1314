b2effcbdbd4e0d223002591c9a9ddc571b5bd3b3
sacry- <nitsche25@gmail.com>
2013-11-05 08:42:51 +0100

    prep stuff

--- GKA/GKA_A1/src/GKA_A3/FordFulkerson.java
+++ GKA/GKA_A1/src/GKA_A3/FordFulkerson.java
@@ -1,5 +1,18 @@
 package GKA_A3;
 
+import GKA_A1.IAIGraph;
+
 public class FordFulkerson {
 
+	private FordFulkerson() {
+	}
+
+	public static IAIGraph fordFulkerson(IAIGraph graph) {
+		return algo(graph);
+	}
+
+	private static IAIGraph algo(IAIGraph graph) {
+		return graph;
+	}
+	
 }
