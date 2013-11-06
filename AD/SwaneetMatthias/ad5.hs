c5badbdde0617d3ee25f0983d03eccc957ea790e
GollyTicker <golly.ticker@gmail.com>
2013-11-06 23:54:59 +0100

    aufgabe 5.2.1 fin

---
+++ AD/SwaneetMatthias/ad5.hs
@@ -0,0 +1,9 @@
+
+
+f 0 = 1
+f 1 = 1
+f 2 = 1
+f n = f (n-1) + (2 * f (n-2)) + (3 * f (n-3))
+
+
+main = putStrLn $ show (map (\x -> (x, f x)) [0..15])
--- AD/SwaneetMatthias/src/ab5_adts/Aufgabe5_2.java
+++ AD/SwaneetMatthias/src/ab5_adts/Aufgabe5_2.java
@@ -1,7 +1,11 @@
 package ab5_adts;
 
 public class Aufgabe5_2 {
-
+	
+	// Aufgabe 5.2.1:
+	// f [0..9]=[1,1,1,,6,26,66,151,361,861]
+	
+	
 	public static void main(String[] args) {
 		
 	}
