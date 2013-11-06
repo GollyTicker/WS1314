a1985dd314dc079c83af39abb14577ae216d8cf5
GollyTicker <golly.ticker@gmail.com>
2013-11-06 23:59:29 +0100

    aufgabe 5.2.2 fin

--- AD/SwaneetMatthias/src/ab5_adts/Aufgabe5_2.java
+++ AD/SwaneetMatthias/src/ab5_adts/Aufgabe5_2.java
@@ -7,7 +7,22 @@ public class Aufgabe5_2 {
 	
 	
 	public static void main(String[] args) {
-		
 	}
 
+	// Aufgabe 5.2.2:
+	private static int f(int n){
+		if(n<0){
+			System.out.println("Whoops! N is too small!");
+			return -1;
+		}
+
+//		f (0|1|2) = 1
+//		f n = f (n-1) + (2 * f (n-2)) + (3 * f (n-3))
+		if(n<3){
+			return 1;
+		}
+		else{
+			return f(n-1) + 2*f(n-2) + 3*f(n-3);
+		}
+	}
 }
