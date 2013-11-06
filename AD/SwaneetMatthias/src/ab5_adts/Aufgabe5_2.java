6d2c884b428d9bd33184ec9c7f53f8ac3a32d365
GollyTicker <golly.ticker@gmail.com>
2013-11-07 00:05:47 +0100

    aufgabe 5.2.3 fin

--- AD/SwaneetMatthias/src/ab5_adts/Aufgabe5_2.java
+++ AD/SwaneetMatthias/src/ab5_adts/Aufgabe5_2.java
@@ -7,6 +7,7 @@ public class Aufgabe5_2 {
 	
 	
 	public static void main(String[] args) {
+		aufgabe5_2_3();
 	}
 
 	// Aufgabe 5.2.2:
@@ -23,6 +24,25 @@ public class Aufgabe5_2 {
 		}
 		else{
 			return f(n-1) + 2*f(n-2) + 3*f(n-3);
+		}
+	}
+
+	// Aufgabe 5.2.3:
+	private static void aufgabe5_2_3() {
+		int prevResult = 1;
+		for(int n=0; true; n++){
+			int result = f(n);
+			
+			// check whether the current result is
+			// smaller than the previous one for overflows
+			// checking for negative numbers isn't sufficient
+			// because the result is too big to become negative
+			if(result < prevResult){
+				System.out.println("overflow at n=" + n);
+				break;
+			}
+			prevResult=result;
+			System.out.println("n="+n+" works! result: " + result);
 		}
 	}
 }
