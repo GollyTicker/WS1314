3cc7745b893b8b188b520238706e2a00462893c3
sacry- <nitsche25@gmail.com>
2013-12-12 16:35:15 +0100

    first commit for Hierholzer

---
+++ GKA/GKA_A1/src/GKA_A4/Hierholzer.java
@@ -0,0 +1,15 @@
+package GKA_A4;
+
+import GKA_A1.IAIGraph;
+
+public class Hierholzer {
+
+	private IAIGraph graph;
+	private String cmp;
+
+	public Hierholzer(IAIGraph graph, String cmp) {
+		this.graph = graph;
+		this.cmp = cmp;
+	}
+
+}
