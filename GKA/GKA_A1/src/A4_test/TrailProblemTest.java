86e5d3c787863a96b8ebae4fcd9d14adac51c5dd
GollyTicker <golly.ticker@gmail.com>
2013-12-12 15:45:24 +0100

    first tests. not finished

--- GKA/GKA_A1/src/A4_test/TrailProblemTest.java
+++ GKA/GKA_A1/src/A4_test/TrailProblemTest.java
@@ -1,5 +1,41 @@
 package A4_test;
 
-public class TrailProblemTest {
+import org.junit.Test;
 
+import GKA_A1.IAIGraph;
+import GraphUtils.JavaParser;
+import GraphUtils.WhichPath;
+
+public class TrailProblemTest {
+	
+	private String distAttr = "km";
+	private IAIGraph yolo;
+	
+	public TrailProblemTest() {
+	}
+	
+	@Test
+	public void NearestInsertionTest1(){
+		loadGraph("graph30.graph");
+		// this graph is the graph the algorithm is presented in the book with.
+		// write here call of nearest inserteion algorithm and its test case
+		
+		// List<> // liste der Kreisknoten
+		
+	}
+
+	@Test
+	public void NearestInsertionTest2(){
+		loadGraph("graph31.graph");
+		// this graph is the graph used in the exercises for the nearest insertion algorithm
+		// write here call of nearest inserteion algorithm and its test case
+	}
+	
+	private void loadGraph(String graphname) {
+		String path = WhichPath.getPath();
+		JavaParser jp = new JavaParser(path + graphname, distAttr);
+		this.yolo = jp.createGraph();
+		System.out.println("\n<======= New Test =======>\n Input Graph: "
+				+ yolo);
+	}
 }
