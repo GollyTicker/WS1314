b6b96e47238d93e900acfa85b67b38b97f59357b
sacry- <nitsche25@gmail.com>
2013-12-12 16:37:12 +0100

    Beschreibung in Hierholzer

--- GKA/GKA_A1/src/GKA_A4/Hierholzer.java
+++ GKA/GKA_A1/src/GKA_A4/Hierholzer.java
@@ -11,5 +11,16 @@ public class Hierholzer {
 		this.graph = graph;
 		this.cmp = cmp;
 	}
+	
+//	Voraussetzung: Sei G=(V,E) ein zusammenhängender Graph, der nur Knoten mit geradem Grad aufweist.
+//		1. Wähle einen beliebigen Knoten v_0 des Graphen und konstruiere von v_0 ausgehend einen 
+//		Unterkreis K in G, der keine Kante in G zweimal durchläuft.
+//		2. Wenn K ein Eulerkreis ist, breche ab. Andernfalls:
+//		3. Vernachlässige nun alle Kanten des Unterkreises K.
+//		4. Am ersten Eckpunkt von K, dessen Grad größer 0 ist, lässt man nun einen weiteren Unterkreis K' 
+//		entstehen, der keine Kante in K durchläuft und keine Kante in G zweimal enthält.
+//		5. Füge in K den zweiten Kreis K' ein, indem der Startpunkt von K' durch alle Punkte von K' 
+//		in der richtigen Reihenfolge ersetzt wird.
+//		6. Nenne jetzt den so erhaltenen Kreis K und fahre bei Schritt 2 fort.
 
 }
