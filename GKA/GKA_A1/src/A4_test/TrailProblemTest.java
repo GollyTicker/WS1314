106b4f17a7e4bd3a088231701cf6507c66665757
sacry- <nitsche25@gmail.com>
2013-12-12 17:19:32 +0100

    Full test for two positive and two negative examples

--- GKA/GKA_A1/src/A4_test/TrailProblemTest.java
+++ GKA/GKA_A1/src/A4_test/TrailProblemTest.java
@@ -1,12 +1,17 @@
 package A4_test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.junit.Test;
 
 import GKA_A1.IAIGraph;
+import GKA_A4.Hierholzer;
 import GKA_A4.NearestInsertion;
 import GraphUtils.JavaParser;
 import GraphUtils.WhichPath;
@@ -19,7 +24,8 @@ public class TrailProblemTest {
 	public TrailProblemTest() {
 	}
 
-	@Test
+	// NearestInsertion
+	// @Test
 	public void NearestInsertionTest1() {
 		loadGraph("graph30.graph");
 		// this graph is the graph the algorithm is presented in the book with.
@@ -34,7 +40,7 @@ public class TrailProblemTest {
 		assertTrue(algo.getMinimum() <= 120);
 	}
 
-	@Test
+	// @Test
 	public void NearestInsertionTest2() {
 		loadGraph("graph31.graph");
 		// this graph is the graph used in the exercises for the nearest
@@ -48,6 +54,63 @@ public class TrailProblemTest {
 		System.out.println("Minimum: " + algo.getMinimum());
 
 		assertTrue(algo.getMinimum() <= 33);
+	}
+
+	// Hierholzer
+	@Test
+	public void TestIsEulerianPath() {
+		loadGraph("graph35.graph");
+		List<Long> eulerpath = new ArrayList<>(Arrays.asList(0L, 1L, 2L, 3L));
+		assertTrue(isEulerianPath(eulerpath));
+		eulerpath = new ArrayList<>(Arrays.asList(0L, 1L, 2L, 1L, 3L));
+		assertTrue(!isEulerianPath(eulerpath));
+	}
+
+	@Test
+	public void TestIsEulerianPath1() {
+		loadGraph("graph35.graph");
+		Hierholzer algo = new Hierholzer(yolo, cmp);
+		List<Long> result = algo.hierholzeEs();
+		assertTrue(isEulerianPath(result));
+	}
+
+	@Test
+	public void TestIsEulerianPath2() {
+		loadGraph("graph36.graph");
+		Hierholzer algo = new Hierholzer(yolo, cmp);
+		List<Long> result = algo.hierholzeEs();
+		assertTrue(!isEulerianPath(result));
+	}
+
+	@Test
+	public void TestIsEulerianPath3() {
+		loadGraph("graph37.graph");
+		Hierholzer algo = new Hierholzer(yolo, cmp);
+		List<Long> result = algo.hierholzeEs();
+		assertTrue(isEulerianPath(result));
+	}
+
+	@Test
+	public void TestIsEulerianPath4() {
+		loadGraph("graph38.graph");
+		Hierholzer algo = new Hierholzer(yolo, cmp);
+		List<Long> result = algo.hierholzeEs();
+		assertTrue(!isEulerianPath(result));
+	}
+	
+
+	private boolean isEulerianPath(List<Long> edges) {
+		if (new HashSet<Long>(edges).size() != edges.size())
+			return false;
+		Set<Long> vertices = new HashSet<>();
+		for (Long eid : edges) {
+			Long source = yolo.getSource(eid);
+			if (yolo.getIncident(source).size() % 2 != 0
+					|| !vertices.add(source)) {
+				return false;
+			}
+		}
+		return true;
 	}
 
 	private void loadGraph(String graphname) {
