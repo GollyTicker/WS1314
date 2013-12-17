540b0901955c39ded44245ddfeaea5093ed8d427
sacry- <nitsche25@gmail.com>
2013-12-17 09:38:00 +0100

    nearest Insertion accesscount

--- GKA/GKA_A1/src/GKA_A4/NearestInsertion.java
+++ GKA/GKA_A1/src/GKA_A4/NearestInsertion.java
@@ -4,12 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 import GKA_A1.IAIGraph;
+import GraphUtils.ITimeSpace;
 
-public class NearestInsertion {
+public class NearestInsertion implements ITimeSpace {
 
 	private final Long NULL_INDEX = -1L;
 	private final int INFINITY = Integer.MAX_VALUE;
 
+	private int accessCount = 0;
 	private final boolean DEBUGMODE = true;
 
 	private IAIGraph graph;
@@ -51,6 +53,7 @@ public class NearestInsertion {
 		int minimum = INFINITY;
 		List<Long> bestCycle = new ArrayList<>();
 		for (int idx = 1; idx < w.size(); idx++) {
+			accessCount += (1 + w.size());
 			List<Long> possibleNextCycles = new ArrayList<>(w);
 			possibleNextCycles.add(idx, vId);
 			int sum = sumCircle(possibleNextCycles);
@@ -68,12 +71,13 @@ public class NearestInsertion {
 	private int sumCircle(List<Long> nextCycle) {
 		int acc = 0;
 		for (int i = 0; i < nextCycle.size() - 1; i++) {
+			accessCount += 2;
 			Long source = nextCycle.get(i);
 			Long target = nextCycle.get(i + 1);
 			// Exception case for W= [A,A]
 			if (source != target) {
-				Long eId = getEdgeBetween(nextCycle.get(i),
-						nextCycle.get(i + 1));
+				accessCount += 1;
+				Long eId = getEdgeBetween(source, target);
 				acc += graph.getValE(eId, cmp);
 			}
 		}
@@ -81,29 +85,35 @@ public class NearestInsertion {
 	}
 
 	private Long findNearestVertice() {
-		Long minimumVertice = NULL_INDEX;
+		// d() has to be called on every remaining element in vertices
+		Long minimum_vertice = NULL_INDEX;
 		int minimum_d = INFINITY;
 		for (Long vId : vertices) {
+			accessCount += 1;
 			int tmpD = d(vId);
 			if (tmpD < minimum_d) {
-				minimumVertice = vId;
+				minimum_vertice = vId;
 			}
 		}
-		return pop(minimumVertice);
+		return pop(minimum_vertice);
 	}
 
 	private int d(Long vId) {
+		// finds the minimum weight from a vertice in w to the current given
+		// vertice vId.
 		int minimum = INFINITY;
 		for (Long wId : w) {
+			accessCount += 1;
 			Long eId = getEdgeBetween(wId, vId);
-			int dist = graph.getValE(eId, cmp);
-			minimum = Math.min(dist, minimum);
+			int distance = graph.getValE(eId, cmp);
+			minimum = Math.min(distance, minimum);
 		}
 		return minimum;
 	}
 
 	public Long getEdgeBetween(Long wId, Long vId) {
 		for (Long eId : graph.getIncident(vId)) {
+			accessCount += 1;
 			if (graph.edgeIsBetween(eId, vId, wId)) {
 				return eId;
 			}
@@ -121,6 +131,7 @@ public class NearestInsertion {
 		int idx = 0;
 		Long head = vertices.get(idx);
 		vertices.remove(idx);
+		accessCount += 2;
 		return head;
 	}
 
@@ -128,6 +139,7 @@ public class NearestInsertion {
 		int idx = vertices.indexOf(vId);
 		Long head = vertices.get(idx);
 		vertices.remove(idx);
+		accessCount += 3;
 		return head;
 	}
 
@@ -138,5 +150,26 @@ public class NearestInsertion {
 	private void debugPrint(String s) {
 		if (DEBUGMODE)
 			System.out.println(s);
+	}
+
+	@Override
+	public int accessCount() {
+		return accessCount;
+	}
+
+	@Override
+	public void setAccessCount(int ac) {
+		accessCount = ac;
+	}
+
+	@Override
+	public void resetAccessCount() {
+		accessCount = 0;
+
+	}
+
+	@Override
+	public void printCount() {
+		System.out.println(accessCount);
 	}
 }
